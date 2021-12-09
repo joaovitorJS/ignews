6b8ed8f27bbf4a8dfe4dd8afff893f0110388e1a
joaoJS-Love <joao.vitordeoliveira@hotmail.com>
2021-12-09 13:51:52 -0400

    static-site-generation-SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -39,7 +39,8 @@ export default function Home({ product }: HomeProps) {
 }
 
 // Servidor Next.js (Camada do next)
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   // Prince de um produto, 'price_1K4pwjFvSm04yL3otOXOkxd9' é o API ID do produto, para consultar basta ver produtos no site Stripe
   const price = await stripe.prices.retrieve('price_1K4pwjFvSm04yL3otOXOkxd9');
 
@@ -54,6 +55,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours 
   }
 }
+
+
