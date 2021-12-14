bcddeb1aadb3705d36ba7030a7f1ea62fcc95bc6
joaoJS-Love <joao.vitordeoliveira@hotmail.com>
2021-12-14 16:07:08 -0400

    menu-navigation-color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../../components/ActiveLink';
 import styles from './styles.module.scss';
 
 export function Header () {
+  const { asPath } = useRouter();
+  
   return (
     <header className={styles.headerContainer}> 
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
